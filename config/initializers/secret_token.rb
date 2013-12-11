05e5a36e201973c6b4ada3f9a70d7a3537600aee
Felix Thea <felixthea@gmail.com>
2013-12-11 13:28:33 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Workwiki::Application.config.secret_token = '4b2046bf1d0fbb2d1a54e113ebb64cc59379ca959e2f53b97229d44559c76618fcf8fdde94666b89fbf824cf3be70394891feb2614713a382821ee4761ece882'
