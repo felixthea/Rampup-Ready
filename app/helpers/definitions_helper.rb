1f8263db4c3a0371f94b2c4dbfe583c3ef071c43
Felix Thea <felixthea@gmail.com>
2013-12-12 11:14:36 -0500

    user can see number of upvotes and downvotes for each definition

--- app/helpers/definitions_helper.rb
+++ app/helpers/definitions_helper.rb
@@ -1,2 +1,21 @@
 module DefinitionsHelper
+
+  def sum_upvotes(definition_id)
+    upvotes = all_votes(definition_id).select { |vote| vote == 1 }
+    upvote_sum = upvotes.inject(0) { |sum, value| sum + value }
+    return upvote_sum
+  end
+
+  def sum_downvotes(definition_id)
+    downvotes = all_votes(definition_id).select { |vote| vote == -1 }
+    downvote_sum = downvotes.inject(0) { |sum, value| sum + value }
+    return downvote_sum
+  end
+
+  def all_votes(definition_id)
+    vote_objects = Vote.find_all_by_definition_id(definition_id)
+    votes = vote_objects.map { |vote_object| vote_object.vote }
+    return votes
+  end
+
 end
--- app/helpers/words_helper.rb
+++ app/helpers/words_helper.rb
@@ -1,2 +1,3 @@
 module WordsHelper
+  include DefinitionsHelper
 end
