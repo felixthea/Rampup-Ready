efc9120018178f02482e493d9888f55aa827341d
Felix Thea <felixthea@gmail.com>
2013-12-31 17:31:20 -0500

    updated configs to support sendgrid emailing

--- config/environment.rb
+++ config/environment.rb
@@ -16,7 +16,5 @@ if Rails.env.production?
   }
   ActionMailer::Base.delivery_method ||= :smtp
 elsif Rails.env.development?
-  # Remember the letter_opener gem? It won't send real emails; it
-  # just opens them in another tab.
   ActionMailer::Base.delivery_method = :letter_opener
 end
