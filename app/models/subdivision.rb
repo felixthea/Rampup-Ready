2912a004d4faf59e19306defb96faa785e5b0547
Felix Thea <felixthea@gmail.com>
2014-03-10 22:07:14 -0400

    added company_id to attr accessible

--- app/models/subdivision.rb
+++ app/models/subdivision.rb
@@ -1,5 +1,5 @@
 class Subdivision < ActiveRecord::Base
-  attr_accessible :name
+  attr_accessible :name, :company_id
 
   validates :name, presence: true
 
--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,5 +1,5 @@
 class Tag < ActiveRecord::Base
-  attr_accessible :name
+  attr_accessible :name, :company_id
 
   validates :name, presence: true, uniqueness: true
 
