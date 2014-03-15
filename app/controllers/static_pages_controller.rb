66e6c42992a3739139408919be8ac903ca5feca6
Felix Thea <felixthea@gmail.com>
2014-03-15 09:39:48 -0400

    initial redesign

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,5 +1,9 @@
 class StaticPagesController < ApplicationController
 	def index
+		@new_words = Word.where('company_id = ?', current_co.id).order("words.created_at desc").first(5)
+		@new_curriculums = Curriculum.where('user_id = ?', current_user.id).order("curriculums.created_at desc").first(5)
+		# new curriculums is wrong.  needs to also grab the public curriculums and not just the ones that belong to the user
+		@new_definitions
 		render :index
 	end
 end
