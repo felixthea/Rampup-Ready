cf86faaa85e929ba8323acf4660833aa73e76a21
Felix Thea <felixthea@gmail.com>
2014-03-16 18:04:19 -0400

    add word from word#index

--- app/controllers/words_controller.rb
+++ app/controllers/words_controller.rb
@@ -9,7 +9,9 @@ class WordsController < ApplicationController
     # @words = Word.order("words.name asc").page(params[:page])
     @curriculums = Curriculum.find_all_by_user_id(current_user.id)
     @word = Word.new
-    @recent = Word.recently_added(current_co, 10);
+    @recent = Word.recently_added(current_co, 10)
+    @subdivisions = Subdivision.where('company_id = ?', current_co.id)
+    @tags = Tag.where('company_id = ?', current_co.id)
     respond_to do |format|
       format.html
       format.json { render json: @words }
