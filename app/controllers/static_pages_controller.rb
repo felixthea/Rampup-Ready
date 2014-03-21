15737287168e5e18d9e631b95213f229e7c3006d
Felix Thea <felixthea@gmail.com>
2014-03-20 21:35:57 -0400

    added log out button

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,4 +1,7 @@
 class StaticPagesController < ApplicationController
+	
+	before_filter :require_current_user!
+
 	def index
 		@new_words = Word.recently_added(current_co, 5)
 		@new_curriculums = Curriculum.recently_added(current_co, 5)
