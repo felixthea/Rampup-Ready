3304f2dfe242b965256f1320a219f3249f340cba
Felix Thea <felixthea@gmail.com>
2014-03-29 16:11:48 -0400

    built unstyled form for company creation

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -1,2 +1,18 @@
 class CompaniesController < ApplicationController
+	def new
+		render :new, layout: "sales"
+	end
+
+	def create
+		@company = Company.new(params[:company])
+		fail
+		if @company.save
+			redirect_to @company
+		else
+			render :new
+		end
+	end
+
+	def destroy
+	end
 end
