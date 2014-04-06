ac109e4ed929a6383cbaeff55da8239a30764b49
Felix Thea <felixthea@gmail.com>
2014-04-06 11:47:38 -0400

    fixed bug where search provides results from other companies

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -13,7 +13,4 @@ class Tag < ActiveRecord::Base
   has_many :tagged_definitions, through: :taggings, source: :definition
   belongs_to :company
 
-  include PgSearch
-  multisearchable against: :name
-
 end
